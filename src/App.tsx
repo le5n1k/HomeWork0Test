474a67ab8534a6044c85efe97edceb5d225e1985
le5n1k <3dimon343@gmail.com>
2023-10-11 18:02:16 +0300

    add testHomeWork

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,20 @@ function App() {
         <div className="App">
             <Box color={myTheme.colors.backgroundColor}>
                 <Card color={myTheme.colors.cardColor}>
-                    <Img src="https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1698019200&Signature=Tw421m5BaCFsNUCPNzLxB3VCbjA2Eg-9u2AJD6CvL5qv91Mb77qV7zyTDyYl~zitqdZtDC5YzR-YNhlOzvFEyw19DiUnbqU7Lf0~Vybe3W~qUJmK~ImRJa29USwSCnltQWilYHju~fcfV5ZrRtBqagqCBrgKgHXm2-D6ATLpE1vsj71XgIKvhSUInSH4jN~pM8BC1jueampadWsGuyDjQznAd8HSyyWkEPT4REYF-mNApgNU3APDNNSV~axA6mjjoWACzIiwUQndU2L3xcPGS3p8eFkrtkLOlp05CCRBMHdaiwWX9J6jsXTYDPh5WtsULdbRbdE29dv7ktdzGxX2zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
+                    <Img
+                        src="https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1698019200&Signature=Tw421m5BaCFsNUCPNzLxB3VCbjA2Eg-9u2AJD6CvL5qv91Mb77qV7zyTDyYl~zitqdZtDC5YzR-YNhlOzvFEyw19DiUnbqU7Lf0~Vybe3W~qUJmK~ImRJa29USwSCnltQWilYHju~fcfV5ZrRtBqagqCBrgKgHXm2-D6ATLpE1vsj71XgIKvhSUInSH4jN~pM8BC1jueampadWsGuyDjQznAd8HSyyWkEPT4REYF-mNApgNU3APDNNSV~axA6mjjoWACzIiwUQndU2L3xcPGS3p8eFkrtkLOlp05CCRBMHdaiwWX9J6jsXTYDPh5WtsULdbRbdE29dv7ktdzGxX2zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
                     <CardTitle>Headline</CardTitle>
-                    <CardText><Pa>Faucibus. Faucibus. Sit sit sapien sit</Pa><Pa> tempusrisu ut. Sit molestie ornare in venen.</Pa></CardText>
+                    <CardText><Pa>Faucibus. Faucibus. Sit sit sapien sit</Pa><Pa> tempusrisu ut. Sit molestie ornare in
+                        venen.</Pa></CardText>
+                    <StyledBtn btnType={"primary"}>See more</StyledBtn>
+                    <StyledBtn btnType={"outlined"}>Save</StyledBtn>
+                </Card>
+                <Card color={myTheme.colors.cardColor}>
+                    <Img
+                        src="https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1698019200&Signature=Tw421m5BaCFsNUCPNzLxB3VCbjA2Eg-9u2AJD6CvL5qv91Mb77qV7zyTDyYl~zitqdZtDC5YzR-YNhlOzvFEyw19DiUnbqU7Lf0~Vybe3W~qUJmK~ImRJa29USwSCnltQWilYHju~fcfV5ZrRtBqagqCBrgKgHXm2-D6ATLpE1vsj71XgIKvhSUInSH4jN~pM8BC1jueampadWsGuyDjQznAd8HSyyWkEPT4REYF-mNApgNU3APDNNSV~axA6mjjoWACzIiwUQndU2L3xcPGS3p8eFkrtkLOlp05CCRBMHdaiwWX9J6jsXTYDPh5WtsULdbRbdE29dv7ktdzGxX2zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
+                    <CardTitle>2 Headline</CardTitle>
+                    <CardText><Pa>Faucibus. Faucibus. Sit sit sapien sit</Pa><Pa> tempusrisu ut. Sit molestie ornare in
+                        venen.</Pa></CardText>
                     <StyledBtn btnType={"primary"}>See more</StyledBtn>
                     <StyledBtn btnType={"outlined"}>Save</StyledBtn>
                 </Card>
@@ -25,10 +36,10 @@ function App() {
     );
 }
 
-const Pa =styled.p `
-&:last-child {
-  margin-top: -12px;
-}
+const Pa = styled.p`
+  &:last-child {
+    margin-top: -12px;
+  }
 `
 export default App;
 
